28824a25e596c82780a8a928547b9ddb20aad3bf
Shoaib <shoaibs411@gmail.com>
2024-01-10 02:37:37 +0530

    Provisioning DB's - Fetching Secrets from AWS Secret Manager

--- data.tf
+++ data.tf
@@ -7,3 +7,14 @@ data "terraform_remote_state" "vpc" {
     region  = "us-east-1"
   }
 }
+
+# Extracting the information of the secrects
+data "aws_secretmanager_secret" "secrets" {
+  name = "roboshop/secrets"
+}
+
+# Extracting the secrect version(value) from the secrets
+data "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id = "data.aws_secretsmanager_secret.secrets.id"  
+}
+
