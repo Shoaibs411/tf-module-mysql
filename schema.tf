ff2f1d791cdc9c344ce2ac2f8f8b18fbb263e12a
Shoaib <shoaibs411@gmail.com>
2024-01-11 00:17:13 +0530

    Provisioning DB's - Injecting mysql schema into RDS DB

---
+++ schema.tf
@@ -0,0 +1,17 @@
+resource "null_resource" "schema" {
+
+# This makes sure that this null_resource will only be executed post the creation of the RDS only
+    depends_on = [aws_db_instance.default]
+
+     provisioner "local-exec" {
+        command = <<EOF
+            cd /tmp
+            curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+            unzip -o mysql.zip
+            cd mysql-main
+            mysql -h aws_db_instance.default.address -uadmin1 -proboshop1 < shipping.sql
+
+        EOF
+    } 
+
+}
